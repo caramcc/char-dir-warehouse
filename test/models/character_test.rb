f3137aa978420c818f0b5c257e16de0e943c468c
Cara McCormack <caramcc@ccs.neu.edu>
2015-03-09 13:16:03 -0400

    add character and user models

---
+++ test/models/character_test.rb
@@ -0,0 +1,11 @@
+require '../test_helper'
+
+class CharacterTest < ActiveSupport::TestCase
+
+  test 'fixtures' do
+
+    assert_equal 1, Character.count
+    assert_equal 30, @arbor[:age]
+
+  end
+end
