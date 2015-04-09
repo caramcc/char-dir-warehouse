0771689636ed9a399162e7a522d9103c97e17994
Cara McCormack <caramcc@ccs.neu.edu>
2015-04-09 01:36:44 -0400

    did some reaping check stuff awhile ago sorry

--- test/models/reaping_check_test.rb
+++ test/models/reaping_check_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class ReapingCheckTest < ActiveSupport::TestCase
+
+  def setup
+    @open.opens_on = Time.now.to_i - 1000
+    @open.closes_on = Time.now.to_i + 1000
+  end
+
   test 'is active' do
     assert @open.is_active?
   end
