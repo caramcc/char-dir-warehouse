87dadd92613586bc0f4545c4e8c81bbdce2249c9
Cara McCormack <caramcc@ccs.neu.edu>
2015-04-12 16:00:31 -0400

    i'm dumb tbh

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,12 @@ class SearchController < ApplicationController
     query = params[:query]
     if query
       if query.split(' ').length == 2
-        character_query = Character.where("`first_name` LIKE '%#{query.split(' ')[0]}%' AND `last_name` LIKE '%#{query.split(' ')[2]}%'")
+        character_query = Character.where("first_name LIKE '%#{query.split(' ')[0]}%' AND last_name LIKE '%#{query.split(' ')[2]}%'")
       else
-        character_query = Character.where("`first_name` LIKE '%#{query}%' OR `last_name` LIKE '%#{query}%'")
+        character_query = Character.where("first_name LIKE '%#{query}%' OR last_name LIKE '%#{query}%'")
       end
 
-      user_query = User.where("`username` LIKE '%#{query}%' OR `display_name` LIKE '%#{query}%'")
+      user_query = User.where("username LIKE '%#{query}%' OR display_name LIKE '%#{query}%'")
 
       @results = {
           :characters => character_query.to_a,
