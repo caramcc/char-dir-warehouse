004b568ac63400e68ba71635449aefd802651600
Cara McCormack <caramcc@ccs.neu.edu>
2015-07-03 10:57:52 -0400

    fire for slack attack

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -380,13 +380,31 @@ class ApiController < ApplicationController
 
   def slack_attack
     text = params.fetch('text').strip.to_i
+    user = params.fetch('user_name')
     #TODO: Validate param
 
     if text < 51
-      r = 'fire, probably'
+      if text < 21 && text > 0
+        r = 'fire extinguished, +0 dmg'
+      elsif text < 35 && text > 20
+        r = 'minor burn, +2 dmg'
+      elsif text < 46 && text > 35
+        r = 'moderate burn, +4 dmg'
+      elsif text < 50 && text > 45
+        r = 'severe burn, +8 dmg'
+      elsif text == 50
+        r = 'immolated, +100 dmg'
+      else
+        "Sorry, I have no idea what #{text} is. Try to do fewer drugs, #{user.upcase}."
+      end
     else
-      attack_data = Attack.find_by_attack_code(text).weaponize
-      r = "Attack #{text} [#{attack_data['weapon']}] is *#{attack_data['text']}*, +#{attack_data['damage']} dmg"
+      attack_data = Attack.find_by_attack_code(text)
+      if attack_data.nil?
+        "Sorry, I have no idea what #{text} is. Try to do fewer drugs, #{user.upcase}."
+      else
+        attack_data = attack_data.weaponize
+        r = "Attack #{text} [#{attack_data['weapon']}] is *#{attack_data['text']}*, +#{attack_data['damage']} dmg"
+      end
     end
 
     render text: r
