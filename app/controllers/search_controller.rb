89847dcb38b092dabb1724739b8f441b0b4f330b
Cara McCormack <caramcc@ccs.neu.edu>
2015-12-27 20:41:03 -0500

    case insensitive search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,9 @@ class SearchController < ApplicationController
     query = params[:query]
     if query
       if query.split(' ').length == 2
-        character_query = Character.where("first_name LIKE '%#{query.split(' ')[0]}%' AND last_name LIKE '%#{query.split(' ')[2]}%'")
+        character_query = Character.where("first_name ILIKE '%#{query.split(' ')[0]}%' AND last_name ILIKE '%#{query.split(' ')[2]}%'")
       else
-        character_query = Character.where("first_name LIKE '%#{query}%' OR last_name LIKE '%#{query}%'")
+        character_query = Character.where("first_name ILIKE '%#{query}%' OR last_name ILIKE '%#{query}%'")
       end
 
       user_query = User.where("username LIKE '%#{query}%' OR display_name LIKE '%#{query}%'")
